00de16a1c9b13a36aef126fe4c03d3144d7d455a
chengguizi <cgz_05@sina.com>
2014-08-13 23:35:48 +0800

    Add code for visualisting the debug values, using Zedgraph

--- GCSViews/CustomLog.Designer.cs
+++ GCSViews/CustomLog.Designer.cs
@@ -34,6 +34,20 @@
             this.myGPSlog = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button1 = new System.Windows.Forms.Button();
+            this.Debuglabel = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.DebugGraph1 = new ZedGraph.ZedGraphControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.currentStateBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.currentStateBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -61,15 +75,87 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // Debuglabel
+            // 
+            resources.ApplyResources(this.Debuglabel, "Debuglabel");
+            this.Debuglabel.Name = "Debuglabel";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.myGPSlog);
+            this.tabPage1.Controls.Add(this.button1);
+            resources.ApplyResources(this.tabPage1, "tabPage1");
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.checkedListBox1);
+            this.tabPage2.Controls.Add(this.DebugGraph1);
+            resources.ApplyResources(this.tabPage2, "tabPage2");
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // DebugGraph1
+            // 
+            resources.ApplyResources(this.DebugGraph1, "DebugGraph1");
+            this.DebugGraph1.Name = "DebugGraph1";
+            this.DebugGraph1.ScrollGrace = 0D;
+            this.DebugGraph1.ScrollMaxX = 0D;
+            this.DebugGraph1.ScrollMaxY = 0D;
+            this.DebugGraph1.ScrollMaxY2 = 0D;
+            this.DebugGraph1.ScrollMinX = 0D;
+            this.DebugGraph1.ScrollMinY = 0D;
+            this.DebugGraph1.ScrollMinY2 = 0D;
+            // 
+            // tabPage3
+            // 
+            resources.ApplyResources(this.tabPage3, "tabPage3");
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick_1);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 10;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.checkedListBox1, "checkedListBox1");
+            this.checkedListBox1.Name = "checkedListBox1";
+            // 
+            // currentStateBindingSource
+            // 
+            this.currentStateBindingSource.DataSource = typeof(MissionPlanner.CurrentState);
+            // 
             // CustomLog
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.myGPSlog);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.Debuglabel);
             this.Controls.Add(this.label1);
             this.Name = "CustomLog";
-            this.Load += new System.EventHandler(this.CustomLog_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.currentStateBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,5 +167,15 @@
         private System.Windows.Forms.TextBox myGPSlog;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Debuglabel;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private ZedGraph.ZedGraphControl DebugGraph1;
+        private System.Windows.Forms.BindingSource currentStateBindingSource;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
     }
 }
--- GCSViews/CustomLog.cs
+++ GCSViews/CustomLog.cs
@@ -9,36 +9,100 @@ using System.Windows.Forms;
 using MissionPlanner.Controls;
 using MissionPlanner.Utilities;
 using System.IO;
+using ZedGraph;
 
 namespace MissionPlanner.GCSViews
 {
     public partial class CustomLog : MyUserControl, IActivate
     {
+        int tickStart = 0;
+
+        List<RollingPointPairList> list = new List<RollingPointPairList>();
+
         public CustomLog()
         {
             InitializeComponent();
+
+            CreateChart(DebugGraph1, "Debug Plot", "Time", "Data");
+
         }
+
+        public void CreateChart(ZedGraphControl zgc, string Title, string XAxis, string YAxis)
+        {
+            GraphPane myPane = zgc.GraphPane;
+
+            // Set the titles and axis labels
+            myPane.Title.Text = Title;
+            myPane.XAxis.Title.Text = XAxis;
+            myPane.YAxis.Title.Text = YAxis;
+
+           // LineItem myCurve;
+
+           /* for (int i=0;i<6;i++)
+            {
+                list.Add(new RollingPointPairList(800));
+                myCurve = myPane.AddCurve("Debug"+i.ToString(), list[i], Color.Aqua, SymbolType.None);
+            }*/
+            
+
+
+            // Show the x axis grid
+            myPane.XAxis.MajorGrid.IsVisible = true;
+
+            /*myPane.XAxis.Scale.Min = 0;
+            myPane.XAxis.Scale.Max = 5;*/
+
+            // Make the Y axis scale red
+            myPane.YAxis.Scale.FontSpec.FontColor = Color.Red;
+            myPane.YAxis.Title.FontSpec.FontColor = Color.Red;
+            // turn off the opposite tics so the Y tics don't show up on the Y2 axis
+            myPane.YAxis.MajorTic.IsOpposite = false;
+            myPane.YAxis.MinorTic.IsOpposite = false;
+            // Don't display the Y zero line
+            myPane.YAxis.MajorGrid.IsZeroLine = true;
+            // Align the Y axis labels so they are flush to the axis
+            myPane.YAxis.Scale.Align = AlignP.Inside;
+            // Manually set the axis range
+            //myPane.YAxis.Scale.Min = -1;
+            //myPane.YAxis.Scale.Max = 1;
+
+            // Fill the axis background with a gradient
+            myPane.Chart.Fill = new Fill(Color.White, Color.LightGray, 45.0f);
+
+            // Sample at 20ms intervals
+            timer2.Enabled = true;
+            timer2.Start();
+
+
+            // Calculate the Axis Scale Ranges
+            zgc.AxisChange();
+
+            tickStart = Environment.TickCount;
+
+
+        }
+
+
 
         public void Activate()
         {
           
         }
 
-        private void CustomLog_Load(object sender, EventArgs e)
-        {
 
-        }
 
-        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        static int timercount = 0;
-        static bool last_armed = true;
+        int timercount = 0;
+        bool last_armed = true;
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            timercount++;
+                ///MainV2.speechEngine.SpeakAsync("Speak test");
+            ///
+            
+            var _mydebug = MainV2.comPort.MAV.cs.mydebug;
+           // if (_mydebug != null && _mydebug.Count > 0)
+               // Debuglabel.Text = _mydebug[0].name + " - " + _mydebug[0].value.ToString();
+            ///
             if (MainV2.comPort.MAV.cs.armed != last_armed)
             {
                 last_armed = MainV2.comPort.MAV.cs.armed;
@@ -62,7 +126,7 @@ namespace MissionPlanner.GCSViews
             mystring += " Mode:" + MainV2.comPort.MAV.cs.mode + " ";
             mystring += " Latitude:" + MainV2.comPort.MAV.cs.lat.ToString() + " Longitude:" + MainV2.comPort.MAV.cs.lng.ToString();
             
-            timercount++;
+            
 
             myGPSlog.AppendText(mystring);
             myGPSlog.AppendText(Environment.NewLine);
@@ -80,5 +144,121 @@ namespace MissionPlanner.GCSViews
             }
             
         }
+
+
+        private void timer2_Tick_1(object sender, EventArgs e)
+        {
+            double time = (Environment.TickCount - tickStart) / 1000.0;
+
+            // Make sure that the curvelist has at least one curve
+            if (DebugGraph1.GraphPane == null || DebugGraph1.GraphPane.CurveList.Count <= 0)
+                return;
+
+            // Get the first CurveItem in the graph
+            LineItem curve = DebugGraph1.GraphPane.CurveList[0] as LineItem;
+            if (curve == null)
+                return;
+
+            // Get the PointPairList
+            IPointListEdit list = curve.Points as IPointListEdit;
+            // If this is null, it means the reference at curve.Points does not
+            // support IPointListEdit, so we won't be able to modify it
+            if (list == null)
+                return;
+
+            // Time is measured in seconds
+            //double time = (Environment.TickCount - tickStart) / 1000.0;
+
+            // Keep the X scale at a rolling 30 second interval, with one
+            // major step between the max X value and the end of the axis
+            Scale xScale = DebugGraph1.GraphPane.XAxis.Scale;
+            if (time > xScale.Max - xScale.MajorStep)
+            {
+                xScale.Max = time + xScale.MajorStep;
+                xScale.Min = xScale.Max - 10.0;
+            }
+
+            // Make sure the Y axis is rescaled to accommodate actual data
+            try
+            {
+                DebugGraph1.AxisChange();
+            }
+            catch { }
+            // Force a redraw
+            DebugGraph1.Invalidate();
+        }
+
+        List<string> displayname = new List<string>();
+
+        Color[] colours = new Color[] {      
+            Color.Red, 
+           Color.Green, 
+           Color.Blue, 
+           Color.Pink, 
+           Color.Yellow, 
+           Color.Orange, 
+           Color.Violet, 
+           Color.Wheat, 
+           Color.Teal, 
+           Color.Silver };
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            var _mydebug = MainV2.comPort.MAV.cs.mydebug;
+
+            if (!MainV2.comPort.BaseStream.IsOpen && !MainV2.comPort.logreadmode)
+                return;
+
+            //Console.WriteLine(DateTime.Now.Millisecond + " timer2 serial");
+            try
+            {
+                MainV2.comPort.MAV.cs.UpdateCurrentSettings(currentStateBindingSource);
+            }
+            catch { }
+
+
+
+            double time = (Environment.TickCount - tickStart) / 1000.0;
+
+
+            if (_mydebug != null && _mydebug.Count > 0)
+            {
+                if (displayname.Count > 0)
+                {
+                    for (int i=0;i<displayname.Count;i++)
+                    {
+                        if(displayname[i].Equals(_mydebug[0].name))
+                        {
+                            if(checkedListBox1.GetItemChecked(i))
+                            {
+                                list[i].Add(time, _mydebug[0].value);
+                                _mydebug.RemoveAt(0);
+                            }
+                            else
+                            {
+                                list[i].Clear();
+                                _mydebug.RemoveAt(0);
+                            }
+                            return;
+                            
+                        }
+                    }
+                }
+                    checkedListBox1.Items.Add(_mydebug[0].name, true);
+                    displayname.Add(_mydebug[0].name);
+                    list.Add(new RollingPointPairList(800));
+                    list[list.Count-1].Add(time, _mydebug[0].value);
+                    DebugGraph1.GraphPane.AddCurve(_mydebug[0].name, list[list.Count - 1], colours[(list.Count - 1) % colours.Length], SymbolType.None);
+                    //LineItem ccurve = (LineItem)(DebugGraph1.GraphPane.CurveList[list.Count - 1]);
+                    //ccurve.Label.Text = _mydebug[0].name;
+                          
+
+                _mydebug.RemoveAt(0);   
+
+            }
+                
+        }
+
+
     }
 }
